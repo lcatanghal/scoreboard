d517f5ffbffb8baa62c36c890adac8f8dbf01305
lcatanghal <luigie.design@gmail.com>
2023-07-11 21:08:17 -0700

    Consolidated code

--- index.js
+++ index.js
@@ -3,47 +3,24 @@
 let homeScoreEl = document.getElementById ("home-score")
 let homeScore = 0
 
-function increaseHomeScore1() {
-    homeScore += 1
-    homeScoreEl.textContent = homeScore
-}
-
-function increaseHomeScore2() {
-    homeScore += 2
-    homeScoreEl.textContent = homeScore
-}
-
-function increaseHomeScore3() {
-    homeScore += 3
-    homeScoreEl.textContent = homeScore
-}
-
-function resetHomeScore() {
-    homeScoreEl.textContent = 0
-    homeScore = 0
-}
-
-//GUEST//
-
 let guestScoreEl = document.getElementById ("guest-score")
 let guestScore = 0
 
-function increaseGuestScore1() {
-    guestScore += 1
-    guestScoreEl.textContent = guestScore
+function increaseHomeScore(increment) {
+    homeScore += increment
+    homeScoreEl.textContent = homeScore
 }
 
-function increaseGuestScore2() {
-    guestScore += 2
-    guestScoreEl.textContent = guestScore
+function increaseAwayScore(incremenet) {
+    awayScore += incremenet
+    awayScoreEl.textContent = awayScore
 }
 
-function increaseGuestScore3() {
-    guestScore += 3
-    guestScoreEl.textContent = guestScore
-}
-
-function resetGuestscore() {
-    guestScoreEl.textContent = 0
-    guestScore = 0
+function resetScore(resetbutton) {
+    if (resetbutton == "home"){
+        homeScoreEl.textContent = 0
+        homeScore = 0;
+    }
+    awayScoreEl.textContent = 0
+    awayScore = 0;
 }
